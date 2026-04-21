002210823b04e7bbbe8c2182cbbda003003ce834
saniya-024 <saniyayadav0208@gmail.com>
2026-04-21 10:14:14 +0530

    add questions of js

---
+++ first.js
@@ -0,0 +1,26 @@
+// questionssss  of variables
+// // let name="Sana";
+// // console.log(name);
+
+
+// let age=25;
+// console.log(age);
+
+// const country="India";
+// console.log(country);
+// //names
+
+// let name = "Saniya";
+
+// console.log("Before:", name);
+
+// name = "Riya";
+
+// console.log("After:", name);
+
+
+const name="Sana";
+console.log("Before:",name);
+ const name= "riya";
+ console.log("after;",name);
+ 
