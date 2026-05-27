7bcebcaa58f9a1dc9ae0ef62f756123bade9a2f3
saniya-024 <saniyayadav0208@gmail.com>
2026-05-27 12:25:30 +0530

    add tectcontent

--- firstt.js
+++ firstt.js
@@ -234,6 +234,8 @@
 
 
 let inp = document.querySelector("input");
+let span = document.querySelector("span");
+
 inp.addEventListener("input", function () {
-    console.log(inp.value.length)
+span.textContent=inp.value.length;
 });
